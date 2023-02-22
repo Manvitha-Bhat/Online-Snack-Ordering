6669a0dd0fd9d617d014f7e074891a14be4f75f6
MANVITHA  BHAT <118717586+Manvitha-Bhat@users.noreply.github.com>
2023-02-22 17:58:21 +0530

    Add files via upload

---
+++ food/localhost.sql
@@ -0,0 +1,429 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 04, 2019 at 01:02 AM
+-- Server version: 5.7.23
+-- PHP Version: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbfood`
+--
+CREATE DATABASE IF NOT EXISTS `dbfood` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dbfood`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbadmin`
+--
+
+CREATE TABLE `tbadmin` (
+  `fld_id` int(10) NOT NULL,
+  `fld_username` varchar(30) NOT NULL,
+  `fld_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbadmin`
+--
+
+INSERT INTO `tbadmin` (`fld_id`, `fld_username`, `fld_password`) VALUES
+(1, 'admin', 'admin@123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbfood`
+--
+
+CREATE TABLE `tbfood` (
+  `food_id` int(11) NOT NULL,
+  `fldvendor_id` int(11) NOT NULL,
+  `foodname` varchar(100) NOT NULL,
+  `cost` bigint(15) NOT NULL,
+  `cuisines` varchar(50) NOT NULL,
+  `paymentmode` varchar(50) NOT NULL,
+  `fldimage` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbfood`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcart`
+--
+
+CREATE TABLE `tblcart` (
+  `fld_cart_id` int(11) NOT NULL,
+  `fld_product_id` bigint(11) NOT NULL,
+  `fld_customer_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomer`
+--
+
+CREATE TABLE `tblcustomer` (
+  `fld_cust_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(30) NOT NULL,
+  `fld_mobile` bigint(10) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblcustomer`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblfood`
+--
+-- Error reading structure for table dbfood.tblfood: #1033 - Incorrect information in file: '.\dbfood\tblfood.frm'
+-- Error reading data for table dbfood.tblfood: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'FROM `dbfood`.`tblfood`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblmessage`
+--
+
+CREATE TABLE `tblmessage` (
+  `fld_msg_id` int(10) NOT NULL,
+  `fld_name` varchar(50) NOT NULL,
+  `fld_email` varchar(50) NOT NULL,
+  `fld_phone` bigint(10) DEFAULT NULL,
+  `fld_msg` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblorder`
+--
+
+CREATE TABLE `tblorder` (
+  `fld_order_id` int(10) NOT NULL,
+  `fld_cart_id` bigint(10) NOT NULL,
+  `fldvendor_id` bigint(10) DEFAULT NULL,
+  `fld_food_id` bigint(10) DEFAULT NULL,
+  `fld_email_id` varchar(50) DEFAULT NULL,
+  `fld_payment` varchar(20) DEFAULT NULL,
+  `fldstatus` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblorder`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvendor`
+--
+
+CREATE TABLE `tblvendor` (
+  `fldvendor_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(50) NOT NULL,
+  `fld_password` varchar(50) NOT NULL,
+  `fld_mob` bigint(10) NOT NULL,
+  `fld_phone` bigint(10) NOT NULL,
+  `fld_address` varchar(50) NOT NULL,
+  `fld_logo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblvendor`
+--
+
+
+--
+-- Indexes for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD PRIMARY KEY (`fld_id`);
+
+--
+-- Indexes for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `fldvendor_id` (`fldvendor_id`);
+
+--
+-- Indexes for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  ADD PRIMARY KEY (`fld_cart_id`);
+
+--
+-- Indexes for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  ADD PRIMARY KEY (`fld_cust_id`);
+
+--
+-- Indexes for table `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  ADD PRIMARY KEY (`fld_msg_id`);
+
+--
+-- Indexes for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  ADD PRIMARY KEY (`fld_order_id`);
+
+--
+-- Indexes for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  ADD PRIMARY KEY (`fldvendor_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  MODIFY `fld_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  MODIFY `fld_cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  MODIFY `fld_cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  MODIFY `fld_msg_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  MODIFY `fld_order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  MODIFY `fldvendor_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  ADD CONSTRAINT `tbfood_ibfk_1` FOREIGN KEY (`fldvendor_id`) REFERENCES `tblvendor` (`fldvendor_id`);
+--
+-- Database: `db_food`
+--
+CREATE DATABASE IF NOT EXISTS `db_food` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `db_food`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbadmin`
+--
+
+CREATE TABLE `tbadmin` (
+  `fld_id` int(11) NOT NULL,
+  `fld_username` varchar(30) NOT NULL,
+  `fld_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvendor`
+--
+
+CREATE TABLE `tblvendor` (
+  `fldvendor_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(50) NOT NULL,
+  `fld_password` varchar(30) NOT NULL,
+  `fld_mob` bigint(10) NOT NULL,
+  `fld_phone` bigint(10) NOT NULL,
+  `fld_address` varchar(100) NOT NULL,
+  `fld_logo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `CustomerID` int(11) NOT NULL,
+  `CustomerName` varchar(250) NOT NULL,
+  `Address` text NOT NULL,
+  `City` varchar(250) NOT NULL,
+  `PostalCode` varchar(30) NOT NULL,
+  `Country` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_customer`
+--
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD PRIMARY KEY (`fld_id`);
+
+--
+-- Indexes for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  ADD PRIMARY KEY (`fldvendor_id`);
+
+--
+-- Indexes for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  MODIFY `fld_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblvendor`
+--
+ALTER TABLE `tblvendor`
+  MODIFY `fldvendor_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_customer`
+--
+ALTER TABLE `tbl_customer`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- Database: `test`
+--
+CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `test`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `OrderNumber` int(11) NOT NULL,
+  `PersonID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reg_data`
+--
+
+-- --------------------------------------------------------
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `PersonID` (`PersonID`);
+
+--
+-- Indexes for table `reg_data`
+--
+
+--
+-- Indexes for table `user_info`
+--
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reg_data`
+--
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`PersonID`) REFERENCES `reg_data` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
